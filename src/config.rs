dda4e23fc85508262c2c61bc9c72d2273171fe3e
Mortoc <Mortoc@gmail.com>
2020-10-20 00:48:41 -0500

    config parses now

--- src/config.rs
+++ src/config.rs
@@ -1,19 +1,33 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Default)]
 pub struct Config {
     pub renderer: RendererConfig,
     pub data_store: DataStoreConfig,
 }
 
-#[derive(Serialize, Deserialize, Copy, Clone)]
+#[derive(Serialize, Deserialize, Clone)]
 #[serde(tag = "type")]
 pub enum RendererConfig {
     WebGPU,
+}
+
+impl Default for RendererConfig {
+    fn default() -> Self {
+        RendererConfig::WebGPU
+    }
 }
 
 #[derive(Serialize, Deserialize, Clone)]
 #[serde(tag = "type")]
 pub enum DataStoreConfig {
     LocalDisk { root: String },
+}
+
+impl Default for DataStoreConfig {
+    fn default() -> Self {
+        DataStoreConfig::LocalDisk {
+            root: "".to_string(),
+        }
+    }
 }
